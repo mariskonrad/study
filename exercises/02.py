1e81bb84778ce6ca318ec90ec20523753f39f304
mariskonrad <marisbelk@gmail.com>
2021-05-26 11:26:17 -0300

    resolved exercises

---
+++ exercises/02.py
@@ -0,0 +1,10 @@
+# create a program that receives 2 numbers and outputs the greater
+
+def find_greater(num1, num2):
+    if num1 > num2:
+        return num1
+    return num2
+
+result = find_greater(23, 67)
+print(result)
+
---
+++ exercises/03.py
@@ -0,0 +1,12 @@
+# create a program that receives a list of numbers and outputs the greater
+
+def find_greater_num(list):
+    return max(list)
+
+find_greater_num(1 2 3 4 5 6)
+print(find_greater_num)
+
+# list = [1, 5, 9, 18, 21, 13]
+# max(list)
+
+# print(max(list))
