9dda4a35b51a04fd8a8680ef408eebbadff0f409
mariskonrad <marisbelk@gmail.com>
2021-07-14 10:14:16 -0300

    resolved exercises

--- MIT-course/18.py
+++ MIT-course/18.py
@@ -1,27 +1,36 @@
 # Problem Set 2:
 # calcular qual é o valor mínimo necessário a ser pago por mês para pagar toda a fatura em 12 meses.
-# neste programa não estaremos lidando com uma taxa mínima de pagamento mensal. 
+# neste programa não estaremos lidando com uma taxa mínima de pagamento mensal.
 # terá as variáveis balance = o saldo devedor no cartão de crédito e anualInterestRate = taxa de juros anual (em decimal)
 # o programa deve printar o menor valor mensal possível para pagar o valor da dívida em 12 meses. Lowest Payment: 180. (e.g.)
 # juros compostos em cima do valor que ainda precisa ser pago cada mes.
 # o pagamento mensal deve ser múltiplo de 10 e o mesmo em todos os meses.
 # é possível que o saldo se torne negativo usando este esquema de pagamento, o que é normal.
 
+# unpaid balance is the balance minus the minimum fixed monthly payment
+# interest is the unpaid balance times annual interest rate divided by 12
+# balance is unpaid balance plus the interest
 
-balance = 1000
-monthlyPayment = 10 
-month = 0
-annualInterestRate = 0.18 / 12.0
-totalPayment = 0
+balance = 3926
+annualInterestRate = 0.2
 
-while month < 12:
-    unpaidBalance = balance - monthlyPayment
-    interest = annualInterestRate * unpaidBalance
-    updatedBalance = unpaidBalance + annualInterestRate  * unpaidBalance
-    totalPayment += monthlyPayment + annualInterestRate
-    month += 1
-    if totalPayment > updatedBalance:
-            print(monthlyPayment)
+minimumMonthlyPayment = 10
+monthlyInterestRate = annualInterestRate / 12.0
+
+while True:
+    month = 0
+    unpaidBalance = balance
+    while month < 12 and unpaidBalance > 0:
+        unpaidBalance -= minimumMonthlyPayment
+        interest = monthlyInterestRate * unpaidBalance
+        unpaidBalance += interest
+        month += 1
+
+    if unpaidBalance <= 0:
+        print(round(minimumMonthlyPayment, 2))
+        break
     else:
-        unpaidBalance = updatedBalance
-        monthlyPayment += 10
+        minimumMonthlyPayment += 10
+
+
+
