6abc25ba57d93a1df3ba84ff0c7e11a05740b770
mariskonrad <marisbelk@gmail.com>
2021-09-02 22:02:41 -0300

    checked user input

--- university-projects/exercise2.py
+++ university-projects/exercise2.py
@@ -1,23 +1,37 @@
+# função que irá criar o e-mail
 def criarEmail(nome, sobrenome, num):
     '''
     Cria um e-mail com a primeira letra do nome do usuário, seu sobrenome e os dois
     últimos números do seu RU.
 
-    nome: string
-    sobrenome: string
-    num: string
+    :nome: string
+    :sobrenome: string
+    :num: string
     '''
-
+    # converter o input do usuário em letras minúsculas
+    nome = nome.lower()
+    sobrenome = sobrenome.lower()
+    # pegar a primeira letra do nome do usuário
     letraNome = nome[0]
-    texto = "Olá, {} {}! Seu e-mail é {}@algoritmos.com.br" .format(nome, sobrenome, letraNome + sobrenome + num)
-    return texto
+    email = letraNome + sobrenome + num + '@algoritmos.com.br'
+    return email
 
 # Programa principal
-
+# Pede o input do usuário e checa se é um input válido
 nome = input("Qual seu nome? ")
-sobrenome = input("Qual seu sobrenome? ")
-num = input("Quais os dois últimos dígitos do seu RU? ")
-print(criarEmail(nome, sobrenome, num))
+while not nome.isalpha():
+    print("Dígitos inválidos. Digite apenas letras.")
+    nome = input("Qual seu nome? ")
 
-# transformar os inputs todos em minusculo
-# colocar o ultimo parametro como opcional
+sobrenome = input("Qual seu sobrenome? ")
+while not sobrenome.isalpha():
+    print("Dígitos inválidos. Digite apenas letras.")
+    sobrenome = input("Qual seu sobrenome? ")
+
+num = input("Quais os dois últimos dígitos do seu RU? ")
+while len(num) != 2 or (not num.isdigit()):
+    print("Dígitos inválidos. Digite apenas 2 números.")
+    num = input("Quais os dois últimos dígitos do seu RU? ")
+
+# Mostra a mensagem para o usuário na tela com o e-mail criado
+print("{}, seu e-mail é ".format(nome) + criarEmail(nome, sobrenome, num))
