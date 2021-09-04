bd7448a4808bfbaf11e8f34f5660e35b7ac0d343
mariskonrad <marisbelk@gmail.com>
2021-09-04 00:06:35 -0300

    defined main function

--- university-projects/exercise3.py
+++ university-projects/exercise3.py
@@ -1,18 +1,49 @@
-listaSorteio = []
+import random
 
-nome = input("Olá! Qual seu nome? ")
-while not nome.isalpha():
-    print("Dígitos inválidos. Digite apenas letras.")
-    nome = input("Olá! Qual seu nome? ")
+# Função que recebe o nome do usuário
+def get_nome():
+    while True:
+        nome = input('Digite o nome: ')
+        if nome.isalpha():
+            return nome
+        print('Dígitos inválidos. Digite apenas letras.')
 
-while True:
-    try:
-        valor = float(input("Digite o valor que deseja doar: "))
-        break
-    except:
-        print("Dígitos inválidos. Digite apenas números.")
-        valor = float(input("Digite o valor que deseja doar: "))
+# Função que recebe o valor a ser doado
+def get_valor():
+    while True:
+        try:
+            valor = float(input('Digite o valor da doação: '))
+            return valor
+        except:
+            print('Dígitos inválidos. Digite apenas números.')
+
+# Função que armazena os nomes dos doadores e realiza o sorteio
+def main():
+    # Programa principal
+    lista_sorteio = []
+    while True:
+        inserir = input('Deseja cadastrar um nome na lista? (S)im / (N)ão ')
+        inserir = inserir.lower()
+        if inserir == 'n':
+            break
+        nome = get_nome()
+        valor = get_valor()
+        print('{} doou R$ {}' .format(nome, valor))
+        vezes_lista = int(valor) // 10
+        lista_sorteio += [nome] * vezes_lista
+        print(lista_sorteio)
+
+    while True:
+        sortear = input('Deseja realizar o sorteio? (S)im / (N)ão ')
+        sortear = sortear.lower()
+        if sortear == 'n':
+            return
+        random.shuffle(lista_sorteio)
+        sorteado = random.choice(lista_sorteio)
+        print('O sorteado foi {}' .format(sorteado))
+        return sorteado
 
 
-# pedir input do usuario ref valor a ser doado
-# a cada 10,00 armazenar o nome da pessoa na listaSorteio
+
+if __name__ == '__main__':
+    main()
