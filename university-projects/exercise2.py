4577d521b03418ddbea4f2d8e24e5456a43b7f78
mariskonrad <marisbelk@gmail.com>
2021-09-03 22:18:39 -0300

    changes double quotes for single quotes

--- university-projects/exercise2.py
+++ university-projects/exercise2.py
@@ -18,20 +18,20 @@ def criarEmail(nome, sobrenome, num):
 
 # Programa principal
 # Pede o input do usuário e checa se é um input válido
-nome = input("Qual seu nome? ")
+nome = input('Qual seu nome? ')
 while not nome.isalpha():
-    print("Dígitos inválidos. Digite apenas letras.")
-    nome = input("Qual seu nome? ")
+    print('Dígitos inválidos. Digite apenas letras.')
+    nome = input('Qual seu nome? ')
 
-sobrenome = input("Qual seu sobrenome? ")
+sobrenome = input('Qual seu sobrenome? ')
 while not sobrenome.isalpha():
-    print("Dígitos inválidos. Digite apenas letras.")
-    sobrenome = input("Qual seu sobrenome? ")
+    print('Dígitos inválidos. Digite apenas letras.')
+    sobrenome = input('Qual seu sobrenome? ')
 
-num = input("Quais os dois últimos dígitos do seu RU? ")
+num = input('Quais os dois últimos dígitos do seu RU? ')
 while len(num) != 2 or (not num.isdigit()):
-    print("Dígitos inválidos. Digite apenas 2 números.")
-    num = input("Quais os dois últimos dígitos do seu RU? ")
+    print('Dígitos inválidos. Digite apenas 2 números.')
+    num = input('Quais os dois últimos dígitos do seu RU? ')
 
 # Mostra a mensagem para o usuário na tela com o e-mail criado
-print("{}, seu e-mail é ".format(nome) + criarEmail(nome, sobrenome, num))
+print('{}, seu e-mail é '.format(nome) + criarEmail(nome, sobrenome, num))
