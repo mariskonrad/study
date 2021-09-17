dc24e71394096dc14a690c855df6314f2f9ef56b
mariskonrad <marisbelk@gmail.com>
2021-09-17 19:21:16 -0300

    exercise is in redo

--- MIT-course/16.py
+++ MIT-course/16.py
@@ -1,31 +1,29 @@
-#  Exercise: is in
-#  We can use the idea of bisection search to determine if a character is in a string, so long as the string is sorted in alphabetical order.
-# First, test the middle character of a string against the character you're looking for (the "test character"). 
-# If they are the same, we are done - we've found the character we're looking for!
-# If they're not the same, check if the test character is "smaller" than the middle character. 
-# If so, we need only consider the lower half of the string; otherwise, we only consider the upper half of the string. 
-# (Note that you can compare characters using Python's < function.)
-# Implement the function isIn(char, aStr) which implements the above idea recursively to test if char is in aStr. char will be a single character and aStr will be a string that is in alphabetical order. 
-# The function should return a boolean value.
-
-#  What happens when the string is of length 1? nao precisa entrar na recursão, porque já da pra ver logo se char está contido em astr. então ja retorna true or false sem precisar iterar.
-# What happens when the test character equals the middle character? nesse caso, podemos ver que char esta contido em astr. podemos retornar true. 
+# Exercise: is in (redo)
+# First test the middle character of a string against the character you're looking for (test char)
+# If they're the same, we're done.
+# If they're not the same, check if test char is smaller than middle char. If so, we need only consider
+# the lower half of the string. Otherwise, we only consider the upper half.
+# Implement the function which implements the above idea recursively.
 
 def isIn(char, aStr):
     '''
     char: a single character
     aStr: an alphabetized string
-    
-    returns: True if char is in aStr; False otherwise
+
+    returns: True if char is in aStr; False otherwise.
     '''
-    if aStr == "" or len(aStr) == 1 and aStr != char :
+    if len(aStr) == 0:
         return False
 
-    mid_char = len(aStr) // 2 
-    if char == aStr[mid_char]:
+    mid = len(aStr) // 2
+
+    if aStr[mid] == char:
         return True
-    
-    if char < aStr[mid_char]:
-        return isIn(char, aStr[:mid_char])
-    
-    return isIn(char, aStr[mid_char:])
+
+    if len(aStr) == 1:
+        return False
+
+    if char > aStr[mid]:
+        return isIn(char, aStr[mid:])
+
+    return isIn(char, aStr[:mid])
